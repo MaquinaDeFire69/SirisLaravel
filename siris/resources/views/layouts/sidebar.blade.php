cd6bb96755dc641a18a304d793c5777084dfe198
MaquinaDeFire69 <8123110051@utchetumal.edu.mx>
2026-03-20 13:07:45 -0500

    Update sidebar.blade.php

--- siris/resources/views/layouts/sidebar.blade.php
+++ siris/resources/views/layouts/sidebar.blade.php
@@ -85,7 +85,7 @@
                 </a>
                 <ul class="submenu ">
                     <li class="submenu-item  ">
-                        <a href="{{ route('sancionados') }}" class="submenu-link">Reportes</a>   
+                        <a href="{{ route('sancionados.sancionados') }}" class="submenu-link">Reportes</a>   
                     </li>
                 </ul>                
             </li>
