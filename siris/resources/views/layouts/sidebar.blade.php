6aad4a3bfa8db7bd85687fb3dc205066012e2d3a
MaquinaDeFire69 <8123110051@utchetumal.edu.mx>
2026-03-23 22:45:16 -0500

    Update sidebar.blade.php

--- siris/resources/views/layouts/sidebar.blade.php
+++ siris/resources/views/layouts/sidebar.blade.php
@@ -99,7 +99,7 @@
                 
                 <ul class="submenu ">                    
                     <li class="submenu-item  ">
-                        <a href="layout-default.html" class="submenu-link">Periodo informe</a>
+                        <a href="{{ route('conf.periodo_informe') }}" class="submenu-link">Periodo informe</a>
                         
                     </li>
                     <li class="submenu-item  ">
