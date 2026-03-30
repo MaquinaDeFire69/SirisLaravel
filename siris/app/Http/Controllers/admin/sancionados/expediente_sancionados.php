3ba90744e1b0dd0cab4a89b42163f21b6f69c3bc
MaquinaDeFire69 <8123110051@utchetumal.edu.mx>
2026-03-29 22:27:32 -0500

    Saca de controlador ReportesController sancionados expediente_sancionados.php

--- siris/app/Http/Controllers/admin/sancionados/expediente_sancionados.php
+++ siris/app/Http/Controllers/admin/sancionados/expediente_sancionados.php
@@ -4,33 +4,41 @@ namespace App\Http\Controllers\admin\sancionados;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\admin\sancionados\ReportesController;
 
 class expediente_sancionados extends Controller
 {
     public function mostrar($id)
-    {
-        // Simulamos la consulta a la base de datos para este expediente en particular
-        $datosExpediente = [
-            'numero' => 'SP-0'.$id.'/2026',
-            'nombre' => 'ARMANDO PADILLA SANCHEZ',
-            'curp' => 'SAPA890514HQRCMR99',
-            'rfc' => 'SAPA890514MR9',
-            'sexo' => 'MASCULINO',
-            'institucion' => 'SABGOB – Secretaría Anticorrupción y Buen Gobierno',
-            'falta_cometida' => 'Abuso de Funciones',
-            'normatividad' => 'Normatividad vigente en la ley',
-            'articulo' => 'Artículo 143 y 144',
-            'fraccion' => 'Fracción 3era. Y 5ta.',
-            'falta' => 'Grave',
-            'fecha_resolucion' => '2025-12-01',
-            'fecha_notificacion' => '2025-12-20',
-            'fecha_inicio_inhab' => '2026-01-15',
-            'fecha_fin_inhab' => '2027-01-15',
-            'anios' => 1,
-            'sanciones' => ['Inhabilitación', 'Económica']
-        ];
+{
+    $sancionados = ReportesController::getSancionados();
 
-        // Redirige a tu vista asignada
-        return view('admin.sancionados.info_expediente', compact('datosExpediente', 'id'));
+    //BUSCAR EL EXPEDIENTE REAL
+    $expediente = $sancionados->firstWhere('id', $id);
+
+    if (!$expediente) {
+        abort(404);
     }
+
+    $datosExpediente = [
+        'numero' => $expediente['expediente'],
+        'nombre' => $expediente['nombre'],
+        'curp' => 'N/A',
+        'rfc' => 'N/A',
+        'sexo' => 'N/A',
+        'institucion' => $expediente['ente'],
+        'falta_cometida' => $expediente['falta'],
+        'normatividad' => 'Normatividad vigente',
+        'articulo' => 'Artículo X',
+        'fraccion' => 'Fracción X',
+        'falta' => 'Grave',
+        'fecha_resolucion' => '2025-12-01',
+        'fecha_notificacion' => '2025-12-20',
+        'fecha_inicio_inhab' => '2026-01-15',
+        'fecha_fin_inhab' => '2027-01-15',
+        'anios' => 1,
+        'sanciones' => explode(',', $expediente['sancion']) // 🔥 importante
+    ];
+
+    return view('admin.sancionados.info_expediente', compact('datosExpediente', 'id'));
+}
 }
