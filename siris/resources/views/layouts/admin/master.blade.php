aac080a2dfdad1f0254bd6703cd451b7585df420
Madox Que <madoxchetumal@gmail.com>
2026-03-31 16:58:25 -0500

    Cambios en las rutas .web (mantener como lo tengo), cambios en los nombres de carpetas y controladores, error 404, cambio en el sidebar y nuevas carpetas en layouts, cambios en master, redireccion a panel informativo como pagina de inicio | vistas completas (espera de retroalimentacion) | Siris v8.3.1

--- siris/resources/views/layouts/admin/master.blade.php
+++ siris/resources/views/layouts/admin/master.blade.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? '' }} {{ $web_title ?? config('app.name') }}</title>
-
-    <link rel="shortcut icon" href="{{ asset('assets/static/images/logo/favicon.svg') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('Siris.svg') }}" type="image/x-icon">
 
     {{-- Sección para estilos antes --}}
     @yield('stylesfirst')
